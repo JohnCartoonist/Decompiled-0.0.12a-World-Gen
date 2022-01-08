5c519084b2d05f4fa1e347d890c532eb7997a4e2
JohnCartoonist <86744606+JohnCartoonist@users.noreply.github.com>
2022-01-08 13:12:47 -0600

    Add files via upload

---
+++ LevelListener.java
@@ -0,0 +1,9 @@
+package com.mojang.minecraft.level;
+
+public interface LevelListener {
+  void tileChanged(int paramInt1, int paramInt2, int paramInt3);
+  
+  void lightColumnChanged(int paramInt1, int paramInt2, int paramInt3, int paramInt4);
+  
+  void allChanged();
+}
